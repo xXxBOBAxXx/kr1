66adedfc01861dd2b8b58248ee92dfd01a095b21
VovaK <bestpestnest@gmail.com>
2022-05-08 11:50:28 +0300

    cosmetic

--- Program.cs
+++ Program.cs
@@ -1,25 +1,13 @@
-﻿string str = Console.ReadLine();
-
+﻿string str = Console.ReadLine() ?? "";
 string[] strs = str.Split(',');
-
 int cntSpace = 0, cntFirstPrint = 0;
-
 foreach (string rslt in strs)
 {
     if (rslt.Length <= 5 + cntSpace)
     {
-        if (cntFirstPrint == 1)
-        {
-            Console.Write("," + rslt);
-        }
-        else if (cntSpace == 1)
-        {
-            Console.Write(rslt.Substring(1));
-        }
-        else
-        {
-            Console.Write(rslt);
-        }
+        if (cntFirstPrint == 1) Console.Write("," + rslt);
+        else if (cntSpace == 1) Console.Write(rslt.Substring(1));
+        else Console.Write(rslt);
         cntFirstPrint = 1;
     }
     cntSpace = 1;
