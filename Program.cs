09880288d05c675ec55dc1fdb35d528255711aa6
VovaK <bestpestnest@gmail.com>
2022-05-08 11:15:48 +0300

    array output

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string str = "\"hello\", \"2\", \"world\", \":-)\"";
+
+string[] strs = str.Split(',');
+
+foreach (string rslt in strs)
+    if (rslt.Trim() != "")
+        Console.WriteLine(rslt);
